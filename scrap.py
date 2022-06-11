0e7f7125f06aee6f79d39e3506aef4cd4809c34b
Sergey Ryapin <rs1180@mail.ru>
2022-06-11 22:49:34 +0500

    Modified get_soup func

--- scrap.py
+++ scrap.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from fake_headers import Headers
 import requests
 import json
 import re
@@ -6,11 +7,9 @@ import re
 
 def get_soup(u: str):
     """Получение данных из запроса. Создание экземпляря BeautifulSoup"""
-    headers = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
-    }
+    headers = Headers(os="win", headers=True).generate()
     src = requests.get(u, headers=headers).text
+    print(headers)
     return BeautifulSoup(src, "lxml")
 
 
